cb79f7b22f3de9fd0a844005d1c85155158d8ddd
Carlos A. Ospina <carlosaospinac@gmail.com>
2017-04-26 22:37:20 -0500

    New style

--- project/static/assets/js/custom.js
+++ project/static/assets/js/custom.js
@@ -3,14 +3,14 @@
     // Navigation scrolls
     $('.navbar-nav li a').bind('click', function(event) {
         $('.navbar-nav li').removeClass('active');
-        $(this).closest('li').addClass('active');
+        $(this).closest('li').removeClass('active');
         var $anchor = $(this);
         var nav = $($anchor.attr('href'));
         if (nav.length) {
         $('html, body').stop().animate({				
             scrollTop: $($anchor.attr('href')).offset().top				
         }, 1500, 'easeInOutExpo');
-        
+
         event.preventDefault();
         }
     });
